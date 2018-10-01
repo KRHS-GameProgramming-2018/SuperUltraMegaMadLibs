769ddfa00af351abc571b2463d58e2c7cb556981
IndyBender <indybender@gmail.com>
2018-10-01 08:09:26 -0400

    Added another line, changed a line of a word prompt

--- story1.py
+++ story1.py
@@ -4,9 +4,10 @@ def playMadlibs():
     friend1 = getWord("Enter a Name: ")
     numAnimals = getNumber("Enter a number: ", 2, 10)
     animals1 = getWord("Enter a plural animal name: ")
-    shoes1 = getWord("Enter a type of shoe: ")
+    shoes1 = getWord("Enter a plural noun: ")
     things1 = getWord("Enter a plural object: ")
     place1 = getWord("Enter a place: ")
+    item1 = getWord("Enter a noun: ")
     output = ""
     output += "One day I was walking with my friend, " + friend1
     output += ", when suddenly " + friend1
@@ -14,7 +15,7 @@ def playMadlibs():
     output += ". We snuck up on them wearing our " + shoes1
     output += ", and took some great pictures with our " + things1
     output += ". Then the " + animals1 + " ran off, and we continued our walk."
-    output += " We then went back to " + place1 + ", and enjoyed the rest of the day."
+    output += " We then went back to " + place1 + ", and enjoyed a nice, hot cup of " + item1 + "."
     
     
     return output
