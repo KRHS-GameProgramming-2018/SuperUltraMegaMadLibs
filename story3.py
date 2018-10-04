4416c1365c7f1a28ea08cc3131c9a56f408f2658
caden gainer <spoodertan@gmail.com>
2018-10-04 08:08:13 -0400

    s3

--- story3.py
+++ story3.py
@@ -2,7 +2,8 @@ from getInput import *
 
 def playMadlibs():
     Person1 = getWord ("please enter a person's name: ")
-    
-    
+    place1 = getWord ("plese enter a place: ")
+    output = ""
     output += "One day I was calling " + Person1 + "on the phone"
-    output += Person1 + "said we should meet up"
+    output += Person1 + "said we should meet up at" + place1
+
