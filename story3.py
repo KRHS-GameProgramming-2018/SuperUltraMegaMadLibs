050ebd8c6b92c71bc88d03072a95c7430e77af9a
caden gainer <spoodertan@gmail.com>
2018-10-10 07:39:18 -0400

    story 3 and 2 chsnged

--- story3.py
+++ story3.py
@@ -5,7 +5,7 @@ def playMadlibs():
     place1 = getWord ("plese enter a place: ")
     object1 = getWord ("please enter an object")
     
-    
+    output = ""
     output += "One day I was calling " + Person1 + "on the phone"
     output += Person1 + "said we should meet up at" + place1
     output += "when I got to" + Place1 + Peron1 + "was holding a(n)" + object1
